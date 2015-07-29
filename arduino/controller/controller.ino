3c40754fe12b8e2cfa3f02a7a4b3c388e0cd79e4
Oliver Barnwell <blastdoors666@gmail.com>
2015-07-29 23:53:21 +0100

    Add experimental

---
+++ arduino/controller/controller.ino
@@ -0,0 +1,99 @@
+//              x, y   x, y    x, y
+int points[] = {0,0, 100,100, 0,100, 100,0, 0,0, 300,200};
+
+int from[2];
+int to[2];
+
+int dx = 0;
+int dy = 0;
+int sx = 0;
+int sy = 0;
+int err = 0;
+
+int toIndex = 0;
+
+int sizeArray = 6;
+
+boolean linedone = true;
+void doBreshnan() {
+
+  if(linedone == true) {
+        if(toIndex > sizeArray) {
+          return;  
+        }
+          toIndex++;
+          from[0] = points[toIndex - 1];
+          from[1] = points[toIndex];
+          to[0] = points[toIndex + 1];
+          to[1] = points[toIndex + 2];
+          dx = abs(to[0] - from[0]);
+          if(from[0] < to[0]) {
+            sx = 1;
+          } else {
+            sx = -1;
+          }
+          dy = abs(to[1] - from[1]);
+          if(from[1] < to[1]) {
+            sy = 1;  
+          } else {
+            sy = -1;
+          }
+          err = (dx>dy ? dx : -dy)/2;
+          linedone = false;
+         }
+        
+       // if(from[0] == to[0] && from[1] == to[1] && toIndex == sizeArray - 1) {
+       //   Serial.print(toIndex == sizeArray - 1);
+       //   linedone = false;
+       //   return;
+       // }
+        
+        if(from[0] == to[0] && from[1] == to[1]) {
+            linedone = true;
+            return;    
+        }
+        
+        double e2 = err;
+        
+        if(e2 > -dx) {
+          err -= dy;
+          from[0] += sx;
+          
+          moveLR(sx);
+        }
+    
+        if(e2 < dy) {
+          err += dx;
+          from[1] += sy;
+          
+          moveUD(sy);        
+        }
+        return;
+  }
+
+int moveUD(int sy) {
+  Serial.print(sy);
+}
+
+int moveLR(int sx) {
+  Serial.print(sx);
+
+}
+
+// the setup routine runs once when you press reset:
+void setup() {                
+  Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+ 
+  delay(25);
+
+    
+  doBreshnan();
+ 
+  
+  
+}
+
